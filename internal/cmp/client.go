41ed142aef6d7866d94dec19b102ffeee786b7b7
glcgadmin <glcgadmin@>
2021-06-09 12:45:53 +0000

    Adding TF provier for snapshot

--- internal/cmp/client.go
+++ internal/cmp/client.go
@@ -7,15 +7,17 @@ import apiClient "github.com/hpe-hcss/vmaas-cmp-go-sdk/pkg/client"
 // Client is the cmp client which will implements all the
 // functions in interface.go
 type Client struct {
-	Instance     Resource
-	Snapshot     Resource
-	Network      DataSource
-	Plan         DataSource
-	Group        DataSource
-	Cloud        DataSource
-	ResourcePool DataSource
-	Layout       DataSource
-	Datastore    DataSource
+	Instance      Resource
+	Snapshot      Resource
+	Network       DataSource
+	Plan          DataSource
+	Group         DataSource
+	Cloud         DataSource
+	ResourcePool  DataSource
+	Layout        DataSource
+	Datastore     DataSource
+	PowerSchedule DataSource
+	Template      DataSource
 }
 
 // NewClient returns configured client
@@ -23,13 +25,15 @@ func NewClient(client *apiClient.APIClient, cfg apiClient.Configuration) *Client
 	return &Client{
 		Instance: newInstance(
 			&apiClient.InstancesApiService{Client: client, Cfg: cfg}),
-		Snapshot:     newSnapshot(&apiClient.InstancesApiService{Client: client, Cfg: cfg}),
-		Network:      newNetwork(&apiClient.NetworksApiService{Client: client, Cfg: cfg}),
-		Plan:         newPlan(&apiClient.PlansApiService{Client: client, Cfg: cfg}),
-		Group:        newGroup(&apiClient.GroupsApiService{Client: client, Cfg: cfg}),
-		Layout:       newLayout(&apiClient.LibraryApiService{Client: client, Cfg: cfg}),
-		Cloud:        newCloud(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
-		ResourcePool: newResourcePool(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
-		Datastore:    newDatastore(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
+		Snapshot:      newSnapshot(&apiClient.InstancesApiService{Client: client, Cfg: cfg}),
+		Network:       newNetwork(&apiClient.NetworksApiService{Client: client, Cfg: cfg}),
+		Plan:          newPlan(&apiClient.PlansApiService{Client: client, Cfg: cfg}),
+		Group:         newGroup(&apiClient.GroupsApiService{Client: client, Cfg: cfg}),
+		Layout:        newLayout(&apiClient.LibraryApiService{Client: client, Cfg: cfg}),
+		Cloud:         newCloud(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
+		ResourcePool:  newResourcePool(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
+		Datastore:     newDatastore(&apiClient.CloudsApiService{Client: client, Cfg: cfg}),
+		PowerSchedule: newPowerSchedule(&apiClient.PowerSchedulesApiService{Client: client, Cfg: cfg}),
+		Template:      newTemplate(&apiClient.VirtualImagesApiService{Client: client, Cfg: cfg}),
 	}
 }
