5ab08ff414b321aed50ea33e6dbb56d808791b97
gandharvas <gandharvas@hpe.com>
2021-07-19 23:18:49 +0530

    add validate func

--- internal/resources/resource_instances.go
+++ internal/resources/resource_instances.go
@@ -264,6 +264,7 @@ func Instances() *schema.Resource {
 				Optional: true,
 				Description: `Restarts the instance if set to any positive integer.
 				Restart works only on pre-created instance.`,
+				ValidateFunc: validation.IntAtLeast(0),
 			},
 		},
 		SchemaVersion:  0,
