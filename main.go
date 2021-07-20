dc71f5674dcc34062fbe95287a36c81905a7dc3a
tshihad <shihad-t@hpe.com>
2021-07-20 02:52:03 -0700

    formatting

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 // (C) Copyright 2021 Hewlett Packard Enterprise Development LP
 
 //go:generate terraform fmt -recursive ./examples/
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 package main
 
