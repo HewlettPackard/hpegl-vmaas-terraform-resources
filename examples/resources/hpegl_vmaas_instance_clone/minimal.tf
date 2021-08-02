da601486ecac744807885cb5b912e5ccf46b59e7
gandharvas <gandharvas@hpe.com>
2021-08-02 12:27:56 +0530

    minor example fixes

--- examples/resources/hpegl_vmaas_instance_clone/minimal.tf
+++ examples/resources/hpegl_vmaas_instance_clone/minimal.tf
@@ -1,7 +1,7 @@
 # (C) Copyright 2021 Hewlett Packard Enterprise Development LP
 
 # minimal instance creation
-resource "hpegl_vmaas_instance" "minimal_instance" {
+resource "hpegl_vmaas_instance_clone" "minimal_instance" {
   name               = "tf_minimal_clone"
   source_instance_id = hpegl_vmaas_instance.tf_instance.id
   network {
