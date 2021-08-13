dbb48920bba1349e0a1d7dc98333fbafd93f8a11
ravikumar29524 <ravi.kumar3@hpe.com>
2021-08-13 18:20:29 +0530

    Resolving errors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hpe-hcss/vmaas-terraform-resources
 go 1.14
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/hashicorp/terraform-plugin-docs v0.4.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.5.0
 	github.com/hewlettpackard/hpegl-provider-lib v0.0.2
--- internal/utils/retry.go
+++ internal/utils/retry.go
@@ -11,6 +11,16 @@ import (
 	"github.com/hpe-hcss/vmaas-terraform-resources/pkg/auth"
 )
 
+type token interface {
+	setScmClientToken(ctx *context.Context, meta interface{})
+}
+type tokenStruct struct {
+}
+
+func (t *tokenStruct) setScmClientToken(ctx *context.Context, meta interface{}) {
+	auth.SetScmClientToken(ctx, meta)
+}
+
 // CondFunc function accepts response and error of the RetryFunc. If any error returns
 // retry will terminated and returns the error
 type CondFunc func(response interface{}, ResponseErr error) (bool, error)
@@ -25,60 +35,76 @@ func defaultCond(resp interface{}, err error) (bool, error) {
 func retry(
 	ctx context.Context,
 	meta interface{},
-	count int,
-	retryDelay time.Duration,
 	fn RetryFunc,
-	cond CondFunc,
-	timeout time.Duration,
+	params CustomRetryParameters,
+	tClient token,
 ) (interface{}, error) {
-	var err error
-	var resp interface{}
 	errorChannel := make(chan error)
 	responseChannel := make(chan interface{})
 	go func(ctx context.Context, meta interface{}, errorChannel chan error, responseChannel chan interface{}) {
 		for i := 0; ; i++ {
-			if i == count {
+			if i == params.retryCount {
 				break
 			}
 			select {
 			case <-ctx.Done():
 				errorChannel <- fmt.Errorf("context timed out")
 				responseChannel <- nil
+
 				return
-			case <-time.After(timeout):
+			case <-time.After(params.timeout):
 				errorChannel <- fmt.Errorf("retry timed out")
 				responseChannel <- nil
+
 				return
 			default:
-				auth.SetScmClientToken(&ctx, meta)
-				resp, err = fn(ctx)
-				c, err := cond(resp, err)
+				tClient.setScmClientToken(&ctx, meta)
+				resp, err := fn(ctx)
+				c, err := params.cond(resp, err)
 				if err != nil {
 					errorChannel <- err
 					responseChannel <- nil
+
 					return
 				}
 				if c {
 					errorChannel <- nil
 					responseChannel <- resp
+
 					return
 				}
 				log.Printf("[WARN] on API call got error: %+v, response: %+v. Retrying", err, resp)
-				time.Sleep(retryDelay)
+				time.Sleep(params.retryDelay)
 			}
 		}
 	}(ctx, meta, errorChannel, responseChannel)
-	err = <-errorChannel
-	resp = <-responseChannel
+	err := <-errorChannel
+	resp := <-responseChannel
 	if err != nil {
 		return resp, err
 	}
+
 	return resp, nil
 }
 
 // Retry with default count and timeout
 func Retry(ctx context.Context, meta interface{}, fn RetryFunc) (interface{}, error) {
-	return retry(ctx, meta, defaultRetryCount, defaultRetryTimeout, fn, defaultCond, defaultTimeout)
+	params := CustomRetryParameters{
+		retryCount: defaultRetryCount,
+		retryDelay: defaultRetryTimeout,
+		cond:       defaultCond,
+		timeout:    defaultTimeout,
+	}
+	tStruct := tokenStruct{}
+
+	return retry(ctx, meta, fn, params, &tStruct)
+}
+
+type CustomRetryParameters struct {
+	retryCount int
+	retryDelay time.Duration
+	cond       CondFunc
+	timeout    time.Duration
 }
 
 // CustomRetry allows developers to configure the timeout, retry count and delay
@@ -96,11 +122,10 @@ func (c *CustomRetry) Retry(
 	meta interface{},
 	fn RetryFunc,
 ) (interface{}, error) {
-
 	if c.RetryCount == 0 {
 		c.RetryCount = defaultRetryCount
 	}
-	if c.RetryDelay == 0 {
+	if c.RetryDelay <= 0 {
 		c.RetryDelay = defaultRetryTimeout
 	}
 	if c.Cond == nil {
@@ -110,6 +135,13 @@ func (c *CustomRetry) Retry(
 		c.RetryCount = -1
 	}
 	time.Sleep(c.Delay)
+	params := CustomRetryParameters{
+		retryCount: c.RetryCount,
+		retryDelay: c.RetryDelay,
+		cond:       c.Cond,
+		timeout:    c.Timeout,
+	}
+	tStruct := tokenStruct{}
 
-	return retry(ctx, meta, c.RetryCount, c.RetryDelay, fn, c.Cond, c.Timeout)
+	return retry(ctx, meta, fn, params, &tStruct)
 }
