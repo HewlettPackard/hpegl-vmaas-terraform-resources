e561445ac3ad3bd90d0e17e4358aef5ab37996d0
manjunath-batakurki <manjunath-batakurki@hpe.com>
2021-11-09 23:57:55 -0800

    Get Network Server Changes

--- internal/cmp/router.go
+++ internal/cmp/router.go
@@ -133,7 +133,7 @@ func (r *router) routerAlignRouterRequest(ctx context.Context, meta interface{},
 		routerReq.NetworkRouter.Config.Tier0Gateways = routerReq.NetworkRouter.TfTier1Config.TfTier0Gateways
 		queryParam[nameKey] = tier1GatewayType
 	}
-	// Get router type
+	// Get Router type
 	rtRetry := utils.CustomRetry{}
 	rtRetry.RetryParallel(ctx, meta, func(ctx context.Context) (interface{}, error) {
 		return r.routerClient.GetRouterTypes(ctx, queryParam)
@@ -141,11 +141,9 @@ func (r *router) routerAlignRouterRequest(ctx context.Context, meta interface{},
 	// Get network service ID
 	nsRetry := utils.CustomRetry{}
 	nsRetry.RetryParallel(ctx, meta, func(ctx context.Context) (interface{}, error) {
-		return r.routerClient.GetNetworkServices(ctx, map[string]string{
-			nameKey: "NSX-T",
-		})
+		return r.routerClient.GetNetworkServices(ctx, nil)
 	})
-
+	// Align Router Type
 	rtResp, err := rtRetry.Wait()
 	if err != nil {
 		return err
@@ -155,18 +153,22 @@ func (r *router) routerAlignRouterRequest(ctx context.Context, meta interface{},
 		return fmt.Errorf(errExactMatch, "router-type")
 	}
 	routerReq.NetworkRouter.Type.ID = routerTypes.NetworkRouterTypes[0].ID
-
+	// Align Network Server
 	nsResp, err := nsRetry.Wait()
 	if err != nil {
 		return err
 	}
 	networkService := nsResp.(models.GetNetworkServicesResp)
-	if len(routerTypes.NetworkRouterTypes) != 1 {
+	if len(routerTypes.NetworkRouterTypes) == 0 {
 		return fmt.Errorf(errExactMatch, "network-service")
 	}
-	routerReq.NetworkRouter.NetworkServer.ID = networkService.NetworkServices[0].ID
-	routerReq.NetworkRouter.NetworkServerID = networkService.NetworkServices[0].ID
-
+	for i, n := range networkService.NetworkServices {
+		if n.TypeName == "NSX-T" {
+			routerReq.NetworkRouter.NetworkServer.ID = networkService.NetworkServices[i].ID
+			routerReq.NetworkRouter.NetworkServerID = networkService.NetworkServices[i].ID
+			break
+		}
+	}
 	routerReq.NetworkRouter.Site.ID = routerReq.NetworkRouter.GroupID
 
 	return nil
