bdfa057cdaad2da704e922e02ed622306ee120a7
tshihad <shihad-t@hpe.com>
2021-11-29 02:22:30 -0800

    Fix acceptance test

--- internal/acceptance_test/resource_router_route_test.go
+++ internal/acceptance_test/resource_router_route_test.go
@@ -3,99 +3,38 @@
 package acceptancetest
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
-	"strconv"
 	"testing"
-	"time"
 
 	api_client "github.com/HewlettPackard/hpegl-vmaas-cmp-go-sdk/pkg/client"
-	pkgutils "github.com/HewlettPackard/hpegl-vmaas-terraform-resources/pkg/utils"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/spf13/viper"
+	"github.com/HewlettPackard/hpegl-vmaas-terraform-resources/pkg/atf"
 )
 
 func TestVmaasRouterRoutePlan(t *testing.T) {
-	pkgutils.SkipAcc(t, "vmaas.resource.router_route")
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config:             testAccResourceRouterRoute(),
-				PlanOnly:           true,
-				ExpectNonEmptyPlan: true,
-			},
-		},
-	})
+	acc := &atf.Acc{
+		PreCheck:     testAccPreCheck,
+		Providers:    testAccProviders,
+		ResourceName: "hpegl_vmaas_router_route",
+	}
+	acc.RunResourcePlanTest(t)
 }
 
 func TestAccResourceRouterRouteCreate(t *testing.T) {
-	pkgutils.SkipAcc(t, "vmaas.resource.router_route")
-	if testing.Short() {
-		t.Skip("Skipping router resource creation in short mode")
-	}
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		CheckDestroy: resource.ComposeTestCheckFunc(
-			checkResourceDestroy(
-				"hpegl_vmaas_router_route.tf_route",
-				func(cl *api_client.APIClient, cfg api_client.Configuration, id int, attr map[string]string,
-				) (interface{}, error) {
-					iClient := api_client.RouterAPIService{
-						Client: cl,
-						Cfg:    cfg,
-					}
-					routerStr := attr["router_id"]
-					routerID, err := strconv.Atoi(routerStr)
-					if err != nil {
-						return nil, err
-					}
+	acc := &atf.Acc{
+		ResourceName: "hpegl_vmaas_router_route",
+		PreCheck:     testAccPreCheck,
+		Providers:    testAccProviders,
+		GetAPI: func(attr map[string]string) (interface{}, error) {
+			cl, cfg := getAPIClient()
+			iClient := api_client.RouterAPIService{
+				Client: cl,
+				Cfg:    cfg,
+			}
+			id := toInt(attr["id"])
+			routerID := toInt(attr["router_id"])
 
-					return iClient.GetSpecificRouterRoute(context.Background(), routerID, id)
-				},
-			),
-		),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccResourceRouterRoute(),
-				Check: resource.ComposeTestCheckFunc(
-					validateResource(
-						"hpegl_vmaas_router_route.tf_route",
-					),
-				),
-			},
+			return iClient.GetSpecificRouterRoute(getAccContext(), routerID, id)
 		},
-	})
-}
+	}
 
-func testAccResourceRouterRoute() string {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	return providerStanza + fmt.Sprintf(`
-	resource "hpegl_vmaas_router_route" "tf_route" {
-		name          = "%s_%d"
-		router_id     = %d
-		description   = "%s"
-		enabled       = %t
-		default_route = %t
-		network       = "%s"
-		next_hop      = "%s"
-		mtu           = "%s"
-		priority      = %d
-	  }
-`,
-		viper.GetString("vmaas.resource.router_route.name"),
-		r.Int63n(999999),
-		viper.GetInt("vmaas.resource.router_route.router_id"),
-		viper.GetString("vmaas.resource.router_route.description"),
-		viper.GetBool("vmaas.resource.router_route.enabled"),
-		viper.GetBool("vmaas.resource.router_route.default_route"),
-		viper.GetString("vmaas.resource.router_route.network"),
-		viper.GetString("vmaas.resource.router_route.next_hop"),
-		viper.GetString("vmaas.resource.router_route.mtu"),
-		viper.GetInt("vmaas.resource.router_route.priority"),
-	)
+	acc.RunResourceTests(t)
 }
