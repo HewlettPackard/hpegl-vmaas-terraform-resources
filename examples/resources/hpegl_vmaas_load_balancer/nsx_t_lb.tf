c3e579ff244c57ffc019f0fd3af1f40acbebcc8a
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-08 18:28:08 +0530

    fixing the issues

--- examples/resources/hpegl_vmaas_load_balancer/nsx_t_lb.tf
+++ examples/resources/hpegl_vmaas_load_balancer/nsx_t_lb.tf
@@ -1,15 +1,16 @@
 # (C) Copyright 2022 Hewlett Packard Enterprise Development LP
 
-resource "hpegl_vmaas_load_balancer" "tf_load_balancer" {
+resource "hpegl_vmaas_load_balancer" "tf_lb" {
   name  = "lab-1"       
-  description  = "creating load balancer for test"
+  description  = "UPDATING load balancer for test"
   enabled      =    true        
-  visibility   = "public"
   resource_permissions {
     all = true
   }
   config {
     admin_state = true
-    tier1  = data.hpegl_vmaas_router.tier0_router.provider_id
+    size = "SMALL"
+    log_level = "INFO"
+    tier1  = data.hpegl_vmaas_tier1_router.tier1_router.provider_id
   }
 }
