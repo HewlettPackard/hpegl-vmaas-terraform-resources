9403da99e6e9db29fa4c88d61c6dc44013cc91e6
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-11 16:33:45 +0530

    fixing the changes

--- internal/cmp/lb_profiles.go
+++ internal/cmp/lb_profiles.go
@@ -40,69 +40,10 @@ func (lb *loadBalancerProfile) Read(ctx context.Context, d *utils.Data, meta int
 
 func (lb *loadBalancerProfile) Create(ctx context.Context, d *utils.Data, meta interface{}) error {
 	setMeta(meta, lb.lbClient.Client)
-	// var createReq models.CreateLBProfile
-	// if err := tftags.Get(d, &createReq.CreateLBProfileReq); err != nil {
-	// 	return err
-	// }
-	createReq := models.CreateLBProfile{
-		CreateLBProfileReq: models.CreateLBProfileReq{
-			Name:        d.GetString("name"),
-			LbID:        d.GetInt("lb_id"),
-			Description: d.GetString("description"),
-			ServiceType: d.GetString("service_type"),
-			ProfileConfig: models.LBProfile{
-				ProfileType:              d.GetString("profile_type"),
-				FastTCPIdleTimeout:       d.GetInt("fast_tcp_idle_timeout"),
-				FastUDPIdleTimeout:       d.GetInt("fast_udp_idle_timeout"),
-				HTTPIdleTimeout:          d.GetInt("http_idle_timeout"),
-				ConnectionCloseTimeout:   d.GetInt("connection_close_timeout"),
-				HaFlowMirroring:          d.GetBool("ha_flow_mirroring"),
-				RequestHeaderSize:        d.GetInt("request_header_size"),
-				ResponseHeaderSize:       d.GetInt("response_header_size"),
-				HTTPsRedirect:            d.GetString("redirection"),
-				XForwardedFor:            d.GetString("x_forwarded_for"),
-				RequestBodySize:          d.GetString("request_body_size"),
-				ResponseTimeout:          d.GetInt("response_timeout"),
-				NtlmAuthentication:       d.GetBool("ntlm_authentication"),
-				SharePersistence:         d.GetBool("share_persistence"),
-				CookieName:               d.GetString("cookie_name"),
-				CookieFallback:           d.GetBool("cookie_fallback"),
-				CookieGarbling:           d.GetBool("cookie_garbling"),
-				CookieMode:               d.GetString("cookie_mode"),
-				CookieType:               d.GetString("cookie_type"),
-				CookieDomain:             d.GetString("cookie_domain"),
-				CookiePath:               d.GetString("cookie_path"),
-				MaxIdleTime:              d.GetInt("max_idle_time"),
-				MaxCookieAge:             d.GetInt("max_cookie_age"),
-				HaPersistenceMirroring:   d.GetBool("ha_persistence_mirroring"),
-				PersistenceEntryTimeout:  d.GetInt("persistence_entry_timeout"),
-				PurgeEntries:             d.GetBool("purge_entries_when_full"),
-				SSLSuite:                 d.GetString("ssl_suite"),
-				SessionCache:             d.GetBool("session_cache"),
-				SessionCacheEntryTimeout: d.GetInt("session_cache_timeout"),
-				PreferServerCipher:       d.GetBool("prefer_server_cipher"),
-				Tag: []models.Tags{
-					{
-						Tag:   d.GetString("tag"),
-						Scope: d.GetString("scope"),
-					},
-				},
-			},
-		},
+	var createReq models.CreateLBProfile
+	if err := tftags.Get(d, &createReq.CreateLBProfileReq); err != nil {
+		return err
 	}
-
-	// if createReq.CreateLBProfileReq.ProfileConfig.ProfileType == "" {
-	// 	if createReq.CreateLBProfileReq.ProfileConfig.ProfileType == ApplicationProfile &&
-	// 		createReq.CreateLBProfileReq.ServiceType == "LBHttpProfile" {
-	// 		createReq.CreateLBProfileReq.ProfileConfig.HTTPIdleTimeout = 15
-	// 	} else if createReq.CreateLBProfileReq.ProfileConfig.ProfileType == ApplicationProfile &&
-	// 		createReq.CreateLBProfileReq.ServiceType == "LBFastTcpProfile" {
-	// 		createReq.CreateLBProfileReq.ProfileConfig.FastTCPIdleTimeout = 1800
-	// 	} else if createReq.CreateLBProfileReq.ProfileConfig.ProfileType == ApplicationProfile &&
-	// 		createReq.CreateLBProfileReq.ServiceType == "LBFastUdpProfile" {
-	// 		createReq.CreateLBProfileReq.ProfileConfig.FastUDPIdleTimeout = 300
-	// 	}
-	// }
 
 	if createReq.CreateLBProfileReq.ProfileConfig.ProfileType == "" {
 		createReq.CreateLBProfileReq.ProfileConfig.ProfileType = ApplicationProfile
@@ -112,7 +53,6 @@ func (lb *loadBalancerProfile) Create(ctx context.Context, d *utils.Data, meta i
 	createReq.CreateLBProfileReq.ProfileConfig.ResponseHeaderSize = ResponseHeaderSize
 	createReq.CreateLBProfileReq.ProfileConfig.ResponseTimeout = ResponseTimeout
 
-	// createReq.CreateLBProfileReq.ProfileConfig.ConnectionCloseTimeout = 59
 	createReq.CreateLBProfileReq.ProfileConfig.FastTCPIdleTimeout = 200
 	createReq.CreateLBProfileReq.ProfileConfig.FastUDPIdleTimeout = 300
 	createReq.CreateLBProfileReq.ProfileConfig.CookieName = "Cookie1"
@@ -170,56 +110,9 @@ func (lb *loadBalancerProfile) Delete(ctx context.Context, d *utils.Data, meta i
 func (lb *loadBalancerProfile) Update(ctx context.Context, d *utils.Data, meta interface{}) error {
 	id := d.GetID()
 	setMeta(meta, lb.lbClient.Client)
-	// var updateReq models.CreateLBProfile
-	// if err := tftags.Get(d, &updateReq.CreateLBProfileReq); err != nil {
-	// 	return err
-	// }
-
-	updateReq := models.CreateLBProfile{
-		CreateLBProfileReq: models.CreateLBProfileReq{
-			Name:        d.GetString("name"),
-			LbID:        d.GetInt("lb_id"),
-			Description: d.GetString("description"),
-			ServiceType: d.GetString("service_type"),
-			ProfileConfig: models.LBProfile{
-				ProfileType:              d.GetString("profile_type"),
-				FastTCPIdleTimeout:       d.GetInt("fast_tcp_idle_timeout"),
-				FastUDPIdleTimeout:       d.GetInt("fast_udp_idle_timeout"),
-				HTTPIdleTimeout:          d.GetInt("http_idle_timeout"),
-				ConnectionCloseTimeout:   d.GetInt("connection_close_timeout"),
-				HaFlowMirroring:          d.GetBool("ha_flow_mirroring"),
-				RequestHeaderSize:        d.GetInt("request_header_size"),
-				ResponseHeaderSize:       d.GetInt("response_header_size"),
-				HTTPsRedirect:            d.GetString("redirection"),
-				XForwardedFor:            d.GetString("x_forwarded_for"),
-				RequestBodySize:          d.GetString("request_body_size"),
-				ResponseTimeout:          d.GetInt("response_timeout"),
-				NtlmAuthentication:       d.GetBool("ntlm_authentication"),
-				SharePersistence:         d.GetBool("share_persistence"),
-				CookieName:               d.GetString("cookie_name"),
-				CookieFallback:           d.GetBool("cookie_fallback"),
-				CookieGarbling:           d.GetBool("cookie_garbling"),
-				CookieMode:               d.GetString("cookie_mode"),
-				CookieType:               d.GetString("cookie_type"),
-				CookieDomain:             d.GetString("cookie_domain"),
-				CookiePath:               d.GetString("cookie_path"),
-				MaxIdleTime:              d.GetInt("max_idle_time"),
-				MaxCookieAge:             d.GetInt("max_cookie_age"),
-				HaPersistenceMirroring:   d.GetBool("ha_persistence_mirroring"),
-				PersistenceEntryTimeout:  d.GetInt("persistence_entry_timeout"),
-				PurgeEntries:             d.GetBool("purge_entries_when_full"),
-				SSLSuite:                 d.GetString("ssl_suite"),
-				SessionCache:             d.GetBool("session_cache"),
-				SessionCacheEntryTimeout: d.GetInt("session_cache_timeout"),
-				PreferServerCipher:       d.GetBool("prefer_server_cipher"),
-				Tag: []models.Tags{
-					{
-						Tag:   d.GetString("tag"),
-						Scope: d.GetString("scope"),
-					},
-				},
-			},
-		},
+	var updateReq models.CreateLBProfile
+	if err := tftags.Get(d, &updateReq.CreateLBProfileReq); err != nil {
+		return err
 	}
 
 	updateReq.CreateLBProfileReq.ProfileConfig.RequestHeaderSize = RequestHeaderSize
@@ -230,10 +123,9 @@ func (lb *loadBalancerProfile) Update(ctx context.Context, d *utils.Data, meta i
 
 	}
 
-	// updateReq.CreateLBProfileReq.ProfileConfig.ConnectionCloseTimeout = 59
 	updateReq.CreateLBProfileReq.ProfileConfig.FastTCPIdleTimeout = 200
 	updateReq.CreateLBProfileReq.ProfileConfig.FastUDPIdleTimeout = 300
-	updateReq.CreateLBProfileReq.ProfileConfig.CookieName = "Cookie1"
+	updateReq.CreateLBProfileReq.ProfileConfig.CookieName = "Cookie"
 	updateReq.CreateLBProfileReq.ProfileConfig.CookieMode = "INSERT"
 	updateReq.CreateLBProfileReq.ProfileConfig.CookieType = "LBPersistenceCookieTime"
 	updateReq.CreateLBProfileReq.ProfileConfig.PersistenceEntryTimeout = 300
