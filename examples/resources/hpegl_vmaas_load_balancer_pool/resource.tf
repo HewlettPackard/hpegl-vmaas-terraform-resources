f8df405060648307a6a7d3c1bc2b5e8517636dd5
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-12 11:31:30 +0530

    updated th resource

--- examples/resources/hpegl_vmaas_load_balancer_pool/resource.tf
+++ examples/resources/hpegl_vmaas_load_balancer_pool/resource.tf
@@ -4,20 +4,14 @@ resource "hpegl_vmaas_load_balancer_pool" "test_lb_pool" {
   lb_id = data.hpegl_vmaas_load_balancer.tf_lb.id 
   name  =  "TEST-POOL"       
   description  = "creating load balancer pool"
-  min_active     = 1
-  vip_balance = "ROUND_ROBIN"
+  min_active_members     = 1
+  algorithm = "ROUND_ROBIN"
   config {
     snat_translation_type = "LBSnatAutoMap"
-    passive_monitor_path = 136
-    active_monitor_paths = 133
+    passive_monitor_path = data.hpegl_vmaas_passive_monitor.tf_lb_monitor
+    active_monitor_paths = data.hpegl_vmaas_active_monitor.tf_lb_monitor
     tcp_multiplexing = false
     tcp_multiplexing_number = 6 
     snat_ip_address = ""
-    member_group {
-      name = "Pushpa-group"
-      path = ""
-      ip_revision_filter = "IPV4"
-      port = 80
-    }
   }
 }
