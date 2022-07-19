758e47c97a0252edc7edc9263ee5d3cf98cbcf7f
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-19 22:50:15 +0530

    updated

--- examples/resources/hpegl_vmaas_load_balancer_monitor/nsx_t_lb_udp_monitor.tf
+++ examples/resources/hpegl_vmaas_load_balancer_monitor/nsx_t_lb_udp_monitor.tf
@@ -6,11 +6,13 @@ resource "hpegl_vmaas_load_balancer_monitor" "tf_UDP-MONITOR" {
   name  =  "tf_UDP-MONITOR"       
   description  = "UDP_MONITOR create using tf"
   type = "LBUdpMonitorProfile"
-  fall_count = 3
-  interval =  5 
-  monitor_port = 80
-  rise_count = 3
-  timeout = 15
-  request_body = "request body data"
-  response_data = "success"
+  udp_monitor {
+   fall_count = 3
+   interval =  5 
+   monitor_port = 80
+   rise_count = 3
+   timeout = 15
+   request_body = "request body data"
+   response_data = "success"
+  }
 }
