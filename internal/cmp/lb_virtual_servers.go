f2c6bcf0f9178495b2bf9e2d7aa1259403f0dfe6
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-21 20:30:28 +0530

    removed the commented codes

--- internal/cmp/lb_virtual_servers.go
+++ internal/cmp/lb_virtual_servers.go
@@ -46,27 +46,6 @@ func (lb *loadBalancerVirtualServer) Update(ctx context.Context, d *utils.Data, 
 		return err
 	}
 
-	// updateReq := models.CreateLBVirtualServers{
-	// 	CreateLBVirtualServersReq: models.CreateLBVirtualServersReq{
-	// 		Description:   d.GetString("description"),
-	// 		LbID:          d.GetInt("lb_id"),
-	// 		VipName:       d.GetString("vip_name"),
-	// 		VipAddress:    d.GetString("vip_address"),
-	// 		VipProtocol:   d.GetString("vip_protocol"),
-	// 		VipPort:       d.GetString("vip_port"),
-	// 		Pool:          d.GetInt("pool"),
-	// 		SSLServerCert: d.GetInt("ssl_server_cert"),
-	// 		SSLCert:       d.GetInt("ssl_cert"),
-	// 		VirtualServerConfig: models.VirtualServerConfig{
-	// 			Persistence:        d.GetString("persistence"),
-	// 			PersistenceProfile: d.GetInt("persistence_profile"),
-	// 			ApplicationProfile: d.GetInt("application_profile"),
-	// 			SSLClientProfile:   d.GetString("ssl_client_profile"),
-	// 			SSLServerProfile:   d.GetString("ssl_server_profile"),
-	// 		},
-	// 	},
-	// }
-
 	retry := &utils.CustomRetry{
 		InitialDelay: time.Second * 15,
 		RetryDelay:   time.Second * 30,
@@ -95,7 +74,6 @@ func (lb *loadBalancerVirtualServer) Create(ctx context.Context, d *utils.Data, 
 	}
 	if !lbVirtualServersResp.Success {
 		return fmt.Errorf(successErr, "creating loadBalancerVirtualServer Virtual Servers")
-		//return fmt.Errorf("%v", createReq.CreateLBVirtualServersReq)
 	}
 
 	createReq.CreateLBVirtualServersReq.ID = lbVirtualServersResp.CreateLBVirtualServersResp.ID
