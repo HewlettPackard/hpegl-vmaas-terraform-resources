dc4420d46c6d2e38efcb647ea674ec1b35f7ed97
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-08-12 12:03:54 +0530

    added the AT for monitor

---
+++ internal/acceptance_test/resource_lb_monitor_test.go
@@ -0,0 +1,41 @@
+// (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+
+package acceptancetest
+
+import (
+	"context"
+	"testing"
+
+	api_client "github.com/HewlettPackard/hpegl-vmaas-cmp-go-sdk/pkg/client"
+	"github.com/HewlettPackard/hpegl-vmaas-terraform-resources/pkg/atf"
+)
+
+func TestVmaasLBMonitorPlan(t *testing.T) {
+	acc := &atf.Acc{
+		PreCheck:     testAccPreCheck,
+		Providers:    testAccProviders,
+		ResourceName: "hpegl_vmaas_load_balancer_monitor",
+	}
+	acc.RunResourcePlanTest(t)
+}
+
+func TestAccResourceLBMonitorCreate(t *testing.T) {
+	acc := &atf.Acc{
+		ResourceName: "hpegl_vmaas_load_balancer_monitor",
+		PreCheck:     testAccPreCheck,
+		Providers:    testAccProviders,
+		GetAPI: func(attr map[string]string) (interface{}, error) {
+			cl, cfg := getAPIClient()
+			iClient := api_client.LoadBalancerAPIService{
+				Client: cl,
+				Cfg:    cfg,
+			}
+			id := toInt(attr["id"])
+			lbID := toInt(attr["lb_id"])
+
+			return iClient.GetSpecificLBMonitor(context.Background(), lbID, id)
+		},
+	}
+
+	acc.RunResourceTests(t)
+}
