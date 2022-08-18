4a881f2af5e23d4b5ee779f08c894b41f026847b
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-08-18 13:41:26 +0530

    updated the fixes

--- internal/acceptance_test/resource_virtual_server_test.go
+++ internal/acceptance_test/resource_virtual_server_test.go
@@ -39,25 +39,3 @@ func TestAccResourceLBVirtualServerCreate(t *testing.T) {
 
 	acc.RunResourceTests(t)
 }
-
-func TestAccResourceLBVirtualServerCreate_virtualserverErr(t *testing.T) {
-	acc := &atf.Acc{
-		ResourceName: "hpegl_vmaas_load_balancer_virtual_server",
-		PreCheck:     testAccPreCheck,
-		Providers:    testAccProviders,
-		Version:      "virtualserver_err",
-		GetAPI: func(attr map[string]string) (interface{}, error) {
-			cl, cfg := getAPIClient()
-			iClient := api_client.LoadBalancerAPIService{
-				Client: cl,
-				Cfg:    cfg,
-			}
-			id := toInt(attr["id"])
-			lbID := toInt(attr["lb_id"])
-
-			return iClient.GetSpecificLBVirtualServer(context.Background(), lbID, id)
-		},
-	}
-
-	acc.RunResourceTests(t)
-}
