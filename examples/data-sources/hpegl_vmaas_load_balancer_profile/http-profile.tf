dca772a7d3eec59ee3d3f33675b93e6d147f4507
manjunath-batakurki <manjunath-batakurki@hpe.com>
2022-08-23 10:54:31 -0700

    Minor fixes

--- examples/data-sources/hpegl_vmaas_load_balancer_profile/http-profile.tf
+++ examples/data-sources/hpegl_vmaas_load_balancer_profile/http-profile.tf
@@ -1,4 +1,4 @@
-(C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#(C) Copyright 2022 Hewlett Packard Enterprise Development LP
 
 data "hpegl_vmaas_load_balancer_profile" "tf_http_profile" {
   lb_id = data.hpegl_vmaas_load_balancer.tf_lb.id
