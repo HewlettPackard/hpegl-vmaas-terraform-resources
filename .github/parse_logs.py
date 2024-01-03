74127d7a5be9055411ff5b8af584d73e8c30057a
Mahesh N <mahesh2@hpe.com>
2024-01-03 13:41:40 +0530

    parse logs

---
+++ .github/parse_logs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# (C) Copyright 2024 Hewlett Packard Enterprise Development LP
+
+import os
+import sys
+
+
+def main(args):
+    n = len(args)
+    print(args)
+    if n != 2:
+        print("Pass the log directory")
+        return 1
+    log_path = args[1]
+    file_content = ''
+    for x in os.listdir(log_path):
+        if x.endswith(".txt"):            
+            with open(os.path.join(log_path, x)) as f:
+                file_content += (f.read())
+    test_count = file_content.count('RUN')
+    pass_count = file_content.count('PASS:')
+    fail_count = file_content.count('FAIL:')
+    print(
+        f"\nTestcases Ran: {test_count}\n"
+        f"Testcases Passed: {pass_count}\n"
+        f"Testcases Failed: {fail_count}\n")
+    return 0
+
+if __name__ == "__main__":
+    exit(main(sys.argv))
