491d9085f6d31fe1ba54323fdf25b49ebed97055
Mahesh N <mahesh2@hpe.com>
2024-01-04 08:53:35 +0530

    improvement

--- .github/parse_logs.py
+++ .github/parse_logs.py
@@ -8,21 +8,25 @@ import sys
 def main(args):
     n = len(args)
     if n != 2:
-        print("Pass the log directory")
+        print("Pass the log directory or txt file path")
         return 1
     log_path = args[1]
     file_content = ''
-    for x in os.listdir(log_path):
-        if x.endswith(".txt"):            
-            with open(os.path.join(log_path, x)) as f:
-                file_content += (f.read())
-    test_count = file_content.count('RUN')
+    if log_path.endswith(".txt"):
+        with open(log_path) as f:
+            file_content += (f.read())
+    else:
+        for x in os.listdir(log_path):
+            if x.endswith(".txt"):            
+                with open(os.path.join(log_path, x)) as f:
+                    file_content += (f.read())
+    test_count = file_content.count('RUN') - file_content.count('SKIP:')
     pass_count = file_content.count('PASS:')
     fail_count = file_content.count('FAIL:')
     print(
-        f"\nTestcases Ran: {test_count}; "
-        f"Testcases Passed: {pass_count}; "
-        f"Testcases Failed: {fail_count}; ")
+        f"\nTestcases Ran: {test_count}; \n"
+        f"Testcases Passed: {pass_count}; \n"
+        f"Testcases Failed: {fail_count}; \n")
     return 0
 
 if __name__ == "__main__":
